f24ff8e043fa391fe61954acc69c17c0787d60f4
Roland Mechler <rmechler@gmail.com>
2016-02-07 22:26:59 -0800

    callback now updates database

--- test.py
+++ test.py
@@ -2,11 +2,36 @@
 
 import time
 from songplayer import SongPlayer
+import rethinkdb as r
 
 def handler(song_time):
 	"""
 	"""
-	print("time: {}".format(int(song_time + 0.5)))
+	# print("time: {}".format(int(song_time + 0.5)))
+	r.table("controllers").get("now_playing").update({"time": song_time}).run(conn)
+
+def create_tables(tables):
+	"""
+	"""
+	existing_tables = r.table_list().run(conn)
+	for table in tables:
+		if table not in existing_tables:
+			r.table_create(table).run(conn)
+
+
+conn = r.connect( "rolandmechler.com", 28015, db="rmechler")
+
+# print(r.db_list().run(conn))
+print(r.table_list().run(conn))
+
+create_tables(['songs', 'controllers'])
+
+r.table("controllers").insert({
+    "id": "now_playing",
+    "title": "Circles",
+    "time": 0,
+    "playing": False
+}, conflict="update").run(conn)
 
 player = SongPlayer('Circles.mp3', callback=handler)
 player.play()
